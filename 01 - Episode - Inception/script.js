8adf5a1f6d56f95f1af77b2c5e843505587f8c13
Kumar Ajit <ajitinfotech28@gmail.com>
2024-12-31 12:32:07 +0530

    Add basics things

---
+++ 01 - Episode - Inception/script.js
@@ -0,0 +1,16 @@
+// Create a new element using JavaScript!
+
+// const welcome = document.getElementById("root");
+// const heading = document.createElement("h1");
+// heading.textContent = "Welcome to the Inception guide!";
+// welcome.appendChild(heading);
+
+// Create a new element using react
+
+const root = document.getElementById("root");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Welcome to the Inception guide! - Code with Ajit"
+);
+ReactDOM.render(heading, root);
