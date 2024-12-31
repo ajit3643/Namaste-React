7fdc8c21eeb48ea14e9e38d3a83cda43c506d60f
Kumar Ajit <ajitinfotech28@gmail.com>
2024-12-31 13:38:15 +0530

    Create nested elements

--- 01-Episode-Inception/script.js
+++ 01-Episode-Inception/script.js
@@ -6,11 +6,29 @@
 // welcome.appendChild(heading);
 
 // Create a new element using react
+/*
+ const root = document.getElementById("root");
+ const heading = React.createElement("h1",{ id: "heading" },
+ "Welcome to the Inception guide by - Code with Ajit");
 
-const root = document.getElementById("root");
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Welcome to the Inception guide by - Code with Ajit"
-);
-ReactDOM.render(heading, root);
+  ReactDOM.render(heading, root);
+
+ */
+
+// Create nested elements using React
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement(
+      "h1",
+      { id: "heading" },
+      "Welcome to the Inception guide by - Code with Ajit"
+    ),
+    React.createElement("h2", { id: "sub-heading" }, "This is a sub-heading"),
+    React.createElement("p", { id: "paragraph" }, "This is a paragraph"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h2", { id: "heading" }, "This is another heading"),
+    React.createElement("p", { id: "paragraph" }, "This is another paragraph"),
+  ]),
+]);
