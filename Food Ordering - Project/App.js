ae31869f8f048d763f9106c11872b19febc2426f
Kumar Ajit <ajitinfotech28@gmail.com>
2025-01-04 21:41:12 +0530

    Updated

--- Food Ordering - Project/App.js
+++ Food Ordering - Project/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", null, "This is heading 1"),
-    React.createElement("p", null, "This is paragraph 1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", null, "This is heading 2"),
-    React.createElement("p", null, "This is paragraph 2"),
-  ]),
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement("h1", null, "This is heading 1"),
+//     React.createElement("p", null, "This is paragraph 1"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", null, "This is heading 2"),
+//     React.createElement("p", null, "This is paragraph 2"),
+//   ]),
+// ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+
