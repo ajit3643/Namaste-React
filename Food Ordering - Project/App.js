032ff305ac5ea0414562be82f59accfbfbd7728c
Kumar Ajit <ajitinfotech28@gmail.com>
2025-01-05 12:02:58 +0530

    Updated

--- Food Ordering - Project/App.js
+++ Food Ordering - Project/App.js
@@ -68,6 +68,30 @@ const Body = () => {
           rating="⭐⭐⭐⭐⭐"
           time="45 min"
         />
+        <Restaurant
+          resName="Well Food"
+          cuisine="Chicken, Asian, Desserts, North-Indian"
+          rating="⭐⭐⭐⭐"
+          time="35 min"
+        />
+        <Restaurant
+          resName="Hungry Hope"
+          cuisine="North-Indian, Asian, Chinese, Fast-Food"
+          rating="⭐⭐⭐⭐⭐"
+          time="45 min"
+        />
+        <Restaurant
+          resName="Burger King"
+          cuisine="Burger, Fast-Food, Soft Drink"
+          rating="⭐⭐⭐⭐⭐"
+          time="40 min"
+        />
+        <Restaurant
+          resName="Sub Way"
+          cuisine="Burger, Fast-Food, Soft Drink"
+          rating="⭐⭐⭐"
+          time="1 hr"
+        />
       </div>
     </div>
   );
