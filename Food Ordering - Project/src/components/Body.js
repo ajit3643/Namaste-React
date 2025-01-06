c868f922ac05b2f5d0cb0cfeb7f6dbd10165953a
Kumar Ajit <ajitinfotech28@gmail.com>
2025-01-06 13:05:17 +0530

    Updated

---
+++ Food Ordering - Project/src/components/Body.js
@@ -0,0 +1,52 @@
+import Restaurant from "./Restaurant";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-container">
+        <input type="text" placeholder="Search for restaurant" />
+        <button>Search</button>
+      </div>
+
+      <div className="restaurant-container">
+        <Restaurant
+          resName="Chinese Wok"
+          cuisine="Chinese, Asian, Desserts"
+          rating="⭐⭐⭐⭐"
+          time="30 min"
+        />
+        <Restaurant
+          resName="KFC"
+          cuisine="Chicken, Asian, Desserts"
+          rating="⭐⭐⭐⭐⭐"
+          time="45 min"
+        />
+        <Restaurant
+          resName="Well Food"
+          cuisine="Chicken, Asian, Desserts, North-Indian"
+          rating="⭐⭐⭐⭐"
+          time="35 min"
+        />
+        <Restaurant
+          resName="Hungry Hope"
+          cuisine="North-Indian, Asian, Chinese, Fast-Food"
+          rating="⭐⭐⭐⭐⭐"
+          time="45 min"
+        />
+        <Restaurant
+          resName="Burger King"
+          cuisine="Burger, Fast-Food, Soft Drink"
+          rating="⭐⭐⭐⭐⭐"
+          time="40 min"
+        />
+        <Restaurant
+          resName="Sub Way"
+          cuisine="Burger, Fast-Food, Soft Drink"
+          rating="⭐⭐⭐"
+          time="1 hr"
+        />
+      </div>
+    </div>
+  );
+};
+export default Body;
---
+++ Food Ordering - Project/src/components/Restaurant.js
@@ -0,0 +1,17 @@
+import { CDN_URL } from "../utils/constants";
+const Restaurant = (props) => {
+  const { resName, cuisine, rating, time } = props;
+
+  return (
+    <div className="restaurant">
+      <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/6e459adbdfed0210fd9bc118d5c46b8a" />
+      <div className="restaurant-details">
+        <h3>{resName}</h3>
+        <p>{rating}</p>
+        <p>{cuisine}</p>
+        <p>{time}</p>
+      </div>
+    </div>
+  );
+};
+export default Restaurant;
